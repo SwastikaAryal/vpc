de386a9461257dc8bbbad035982b52068190b431
SwastikaAryal <129056772+SwastikaAryal@users.noreply.github.com>
2023-03-28 11:52:26 +0545

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,9 @@
 output "vpc_id"{
     value = aws_vpc.sa_vpc.id
 }
-output "" {
-  
+output "public_subnet" {
+  value = aws_subnet.sa_publicsubnet.id
+}
+output "private_subnet" {
+  value = aws_subnet.sa_privatesubnet.id
 }
